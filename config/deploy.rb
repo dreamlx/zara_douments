d0a67d9550e446407f857860303bf526a74d72d6
ken-wong <wangkun025@gmail.com>
2020-09-11 11:57:21 +0800

    More shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:dreamlx/zara_douments.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "db/development.sqlite3"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
